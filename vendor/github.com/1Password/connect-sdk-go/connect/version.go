02f145aa1f8264f7e81c34fde8b4b4cffac83d86
Joris Coenen <joris.coenen@agilebits.com>
2021-06-07 14:51:06 +0200

    Fix vendoring of connect-sdk-go

--- vendor/github.com/1Password/connect-sdk-go/connect/version.go
+++ vendor/github.com/1Password/connect-sdk-go/connect/version.go
@@ -2,4 +2,4 @@ package connect
 
 // SDKVersion is the latest Semantic Version of the library
 // Do not rename this variable without changing the regex in the Makefile
-const SDKVersion = "1.0.1"
+const SDKVersion = "1.1.0"
