e0d47f149eadd83a22cdf7b7a89b979b14f9c4ca
William Park <william.park@agilebits.com>
2023-10-17 07:59:12 -0700

    Improve item section and field ID generation

--- onepassword/resource_onepassword_item.go
+++ onepassword/resource_onepassword_item.go
@@ -546,19 +546,17 @@ func dataToItem(data *schema.ResourceData) (*onepassword.Item, error) {
 		if !ok {
 			return nil, fmt.Errorf("Unable to parse section: %v", sections[i])
 		}
-		sid, err := uuid.GenerateUUID()
-		if err != nil {
-			return nil, fmt.Errorf("Unable to generate a section id: %w", err)
-		}
 
-		if section["id"].(string) != "" {
-			sid = section["id"].(string)
-		} else {
+		if section["id"].(string) == "" {
+			sid, err := uuid.GenerateUUID()
+			if err != nil {
+				return nil, fmt.Errorf("Unable to generate a section id: %w", err)
+			}
 			section["id"] = sid
 		}
 
 		s := &onepassword.ItemSection{
-			ID:    sid,
+			ID:    section["id"].(string),
 			Label: section["label"].(string),
 		}
 		item.Sections = append(item.Sections, s)
@@ -568,6 +566,14 @@ func dataToItem(data *schema.ResourceData) (*onepassword.Item, error) {
 			field, ok := sectionFields[j].(map[string]interface{})
 			if !ok {
 				return nil, fmt.Errorf("Unable to parse section field: %v", sectionFields[j])
+			}
+
+			if field["id"].(string) == "" {
+				fid, err := uuid.GenerateUUID()
+				if err != nil {
+					return nil, fmt.Errorf("Unable to generate a field id: %w", err)
+				}
+				field["id"] = fid
 			}
 
 			f := &onepassword.ItemField{
