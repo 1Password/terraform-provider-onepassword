11c1cd1b522665c1461cc2aa1d3d2e8f48f78d3f
Eddy Filip <eddy.filip@agilebits.com>
2024-02-27 10:58:36 +0100

    Add tests for new functionality

--- onepassword/cli/utils_test.go
+++ onepassword/cli/utils_test.go
@@ -148,3 +148,29 @@ func TestPasswordRecipeToString(t *testing.T) {
 		})
 	}
 }
+
+func TestMakeBuildVersion(t *testing.T) {
+	tests := map[string]struct {
+		input    string
+		expected string
+	}{
+		"test stable version": {
+			input:    "1.5.6",
+			expected: "1050601",
+		},
+		"test beta version": {
+			input:    "0.1.0-beta.01",
+			expected: "0010001",
+		},
+	}
+
+	for name, tc := range tests {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			actualBuildVersion := makeBuildVersion(tc.input)
+			if tc.expected != actualBuildVersion {
+				t.Errorf("Unexpected version build number. Expected \"%s\", but got \"%s\"", tc.expected, actualBuildVersion)
+			}
+		})
+	}
+}
