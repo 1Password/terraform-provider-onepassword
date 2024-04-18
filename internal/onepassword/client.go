9babfe68d4a10b5b0e7e05e347435fdf9d9583fa
Eddy Filip <eddy.filip@agilebits.com>
2024-04-18 17:01:23 +0200

    Implement new generic client
    This will return either the Connect or the CLI client.

--- internal/onepassword/client.go
+++ internal/onepassword/client.go
@@ -2,8 +2,11 @@ package onepassword
 
 import (
 	"context"
+	"errors"
 
 	"github.com/1Password/connect-sdk-go/onepassword"
+	"github.com/1Password/terraform-provider-onepassword/internal/onepassword/cli"
+	"github.com/1Password/terraform-provider-onepassword/internal/onepassword/connect"
 )
 
 // Client is a subset of connect.Client with context added.
@@ -23,4 +26,13 @@ type ClientConfig struct {
 	ServiceAccountToken string
 	Account             string
 	OpCLIPath           string
+}
+
+func NewClient(config ClientConfig) (Client, error) {
+	if config.ServiceAccountToken != "" || config.Account != "" {
+		return cli.NewClient(config.ServiceAccountToken, config.Account, config.OpCLIPath), nil
+	} else if config.ConnectHost != "" && config.ConnectToken != "" {
+		return connect.NewClient(config.ConnectHost, config.ConnectToken, config.OpCLIPath), nil
+	}
+	return nil, errors.New("Invalid provider configuration. Either Connect credentials (\"token\" and \"url\") or Service Account (\"service_account_token\" or \"account\") credentials should be set.")
 }
