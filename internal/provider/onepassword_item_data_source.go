967111a6b16b80396f1a8359e9e5aefefd485b1c
Eddy Filip <eddy.filip@agilebits.com>
2024-04-19 10:12:18 +0200

    Improve error message for item data source

--- internal/provider/onepassword_item_data_source.go
+++ internal/provider/onepassword_item_data_source.go
@@ -247,7 +247,7 @@ func (d *OnePasswordItemDataSource) Configure(ctx context.Context, req datasourc
 	if !ok {
 		resp.Diagnostics.AddError(
 			"Unexpected Data Source Configure Type",
-			fmt.Sprintf("Expected *http.Client, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+			fmt.Sprintf("Expected onepassword.Client, got: %T. Please report this issue to the provider developers.", req.ProviderData),
 		)
 
 		return
