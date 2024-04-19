609f941b94f0cabd148f08f5a5ae6eac08e937f2
Eddy Filip <eddy.filip@agilebits.com>
2024-04-19 17:56:45 +0200

    Fix go generate command for the docs

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ import (
 
 // Run the docs generation tool, check its repository for more information on how it works and how docs
 // can be customized.
-//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate -provider-name registry.terraform.io/1password/onepassword
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate -provider-name onepassword
 
 var (
 	// these will be set by the goreleaser configuration
