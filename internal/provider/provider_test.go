3b6aa29885f26e67eae3f16df069043ca2fd4558
jillianwilson <jillian.morgan@agilebits.com>
2024-04-23 09:47:57 -0300

    Remove unused code from proivder_test.go

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -2,24 +2,9 @@ package provider
 
 import (
 	"fmt"
-	"testing"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
-)
-
-const (
-	providerConfig = `terraform {
-	required_providers {
-	  onepassword = {
-		source  = "1Password/onepassword"
-		version = "~> 1.3.0"
-	  }
-	}
-  }
-  # Configure the connection details for the Inventory service
-  provider "onepassword" {
-  }`
 )
 
 // testAccProtoV6ProviderFactories are used to instantiate a provider during
@@ -28,12 +13,6 @@ const (
 // reattach.
 var testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
 	"onepassword": providerserver.NewProtocol6WithError(New("test")()),
-}
-
-func testAccPreCheck(t *testing.T) {
-	// You can add code here to run prior to any test case execution, for example assertions
-	// about the appropriate environment variables being set are common to see in a pre-check
-	// function.
 }
 
 func testAccProviderConfig(url string) string {
