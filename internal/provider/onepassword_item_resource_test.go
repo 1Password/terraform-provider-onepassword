b7c3dbe1400a000fb76117aa38ec4dc14ae28752
jillianwilson <jillian.morgan@agilebits.com>
2024-04-23 10:02:44 -0300

    Created item resource config for database item for testing

--- internal/provider/onepassword_item_resource_test.go
+++ internal/provider/onepassword_item_resource_test.go
@@ -1,62 +1,76 @@
 package provider
 
-// import (
-// 	"fmt"
-// 	"testing"
+import (
+	"fmt"
+	"testing"
 
-// 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
-// )
+	"github.com/1Password/connect-sdk-go/onepassword"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
 
-// func TestAccExampleResource(t *testing.T) {
-// 	resource.Test(t, resource.TestCase{
-// 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
-// 		Steps: []resource.TestStep{
-// 			// Read testing
-// 			{
-// 				Config: testAccExampleResourceConfig("one"),
-// 				Check: resource.ComposeAggregateTestCheckFunc(
-// 					resource.TestCheckResourceAttr("scaffolding_example.test", "configurable_attribute", "one"),
-// 					resource.TestCheckResourceAttr("scaffolding_example.test", "defaulted", "example value when not configured"),
-// 					resource.TestCheckResourceAttr("scaffolding_example.test", "id", "example-id"),
-// 				),
-// 			},
-// 			// ImportState testing
-// 			{
-// 				ResourceName:      "scaffolding_example.test",
-// 				ImportState:       true,
-// 				ImportStateVerify: true,
-// 				// This is not normally necessary, but is here because this
-// 				// example code does not have an actual upstream service.
-// 				// Once the Read method is able to refresh information from
-// 				// the upstream service, this can be removed.
-// 				ImportStateVerifyIgnore: []string{"configurable_attribute", "defaulted"},
-// 			},
-// 			// Update and Read testing
-// 			{
-// 				Config: testAccExampleResourceConfig("two"),
-// 				Check: resource.ComposeAggregateTestCheckFunc(
-// 					resource.TestCheckResourceAttr("scaffolding_example.test", "configurable_attribute", "two"),
-// 				),
-// 			},
-// 			// Delete testing automatically occurs in TestCase
-// 		},
-// 	})
-// }
+func TestAccExampleResource(t *testing.T) {
+	expectedItem := generateDatabaseItem()
+	expectedVault := onepassword.Vault{
+		ID:          expectedItem.Vault.ID,
+		Name:        "Name of the vault",
+		Description: "This vault will be retrieved",
+	}
 
-// func testAccDataBaseResourceConfig(configurableAttribute string) string {
-// 	return fmt.Sprintf(`
-// 	resource "onepassword_item" "demo-database" {
-// 		vault = data.onepassword_vault.acceptance-tests.uuid
+	testServer := setupTestServer(expectedItem, expectedVault, t)
+	defer testServer.Close()
 
-// 		title = "Demo TF Resource - Database"
-// 		category = "database"
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccProviderConfig(testServer.URL) + testAccDataBaseResourceConfig(expectedItem),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("scaffolding_example.test", "configurable_attribute", "one"),
+					resource.TestCheckResourceAttr("scaffolding_example.test", "defaulted", "example value when not configured"),
+					resource.TestCheckResourceAttr("scaffolding_example.test", "id", "example-id"),
+				),
+			},
+			// // ImportState testing
+			// {
+			// 	ResourceName:      "scaffolding_example.test",
+			// 	ImportState:       true,
+			// 	ImportStateVerify: true,
+			// 	// This is not normally necessary, but is here because this
+			// 	// example code does not have an actual upstream service.
+			// 	// Once the Read method is able to refresh information from
+			// 	// the upstream service, this can be removed.
+			// 	ImportStateVerifyIgnore: []string{"configurable_attribute", "defaulted"},
+			// },
+			// // Update and Read testing
+			// {
+			// 	Config: testAccDataBaseResourceConfig("two"),
+			// 	Check: resource.ComposeAggregateTestCheckFunc(
+			// 		resource.TestCheckResourceAttr("scaffolding_example.test", "configurable_attribute", "two"),
+			// 	),
+			// },
+			// // Delete testing automatically occurs in TestCase
+		},
+	})
+}
 
-// 		type = "MySQL"
-// 		hostname = "http://my.fancydomain.com"
-// 		port = "3000"
-// 		database = "badass"
-// 		username = "theboss"
-// 		password_recipe {}
-// 	}
-// `, configurableAttribute)
-// }
+func testAccDataBaseResourceConfig(expectedItem *onepassword.Item) string {
+	return fmt.Sprintf(`
+	data "onepassword_vault" "test-vault {
+		uuid = "%s"
+	}
+
+	resource "onepassword_item" "test-database" {
+		vault = data.onepassword_vault.test-vault.uuid
+
+		title = %s
+		category = %s
+
+		username = %s
+		password_recipe {}
+		hostname = %s
+		database = %s
+		port = %s
+		type = %s
+	}
+`, expectedItem.Vault.ID, expectedItem.Title, expectedItem.Category, expectedItem.Fields[0].Value, expectedItem.Fields[1].Value, expectedItem.Fields[2].Value, expectedItem.Fields[3].Value, expectedItem.Fields[4].Value)
+}
