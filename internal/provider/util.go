2461455b811811209107ab7b87e0344cb5259e3b
Eddy Filip <eddy.filip@agilebits.com>
2024-04-24 10:52:38 +0200

    Add function for better setting a string value
    Terraform expects an attribute to be null if it's not configured. Setting a string to `""` will cause an inconsistency error.
    
    Therefore, we made a special function that properly sets the attribute to null if it is null.

--- internal/provider/util.go
+++ internal/provider/util.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 
 	op "github.com/1Password/connect-sdk-go/onepassword"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 )
 
 func vaultTerraformID(vault *op.Vault) string {
@@ -12,4 +14,11 @@ func vaultTerraformID(vault *op.Vault) string {
 
 func itemTerraformID(item *op.Item) string {
 	return fmt.Sprintf("vaults/%s/items/%s", item.Vault.ID, item.ID)
+}
+
+func setStringValue(value string) basetypes.StringValue {
+	if value == "" {
+		return types.StringNull()
+	}
+	return types.StringValue(value)
 }
