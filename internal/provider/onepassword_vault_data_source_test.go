f45f3c8b710b945293fc10a47e1ea33ee9d02e51
Eddy Filip <eddy.filip@agilebits.com>
2024-05-02 23:53:04 +0200

    Add tests for vault data source
    Co-authored-by: jillianwilson <jillian.morgan@agilebits.com>

--- internal/provider/onepassword_vault_data_source_test.go
+++ internal/provider/onepassword_vault_data_source_test.go
@@ -4,29 +4,43 @@
 package provider
 
 import (
+	"fmt"
 	"testing"
 
+	op "github.com/1Password/connect-sdk-go/onepassword"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
-func TestAccExampleDataSource(t *testing.T) {
+func TestAccVaultDataSource(t *testing.T) {
+	expectedItem := generateDatabaseItem()
+	expectedVault := op.Vault{
+		ID:          expectedItem.Vault.ID,
+		Name:        "Name of the vault",
+		Description: "This vault will be retrieved",
+	}
+
+	testServer := setupTestServer(expectedItem, expectedVault, t)
+	defer testServer.Close()
+
 	resource.Test(t, resource.TestCase{
-		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
-			// Read testing
 			{
-				Config: testAccExampleDataSourceConfig,
+				Config: testAccProviderConfig(testServer.URL) + testAccVaultDataSourceConfig(expectedItem.Vault.ID),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("data.scaffolding_example.test", "id", "example-id"),
+					resource.TestCheckResourceAttr("data.onepassword_vault.test", "id", fmt.Sprintf("vaults/%s", expectedVault.ID)),
+					resource.TestCheckResourceAttr("data.onepassword_vault.test", "uuid", expectedVault.ID),
+					resource.TestCheckResourceAttr("data.onepassword_vault.test", "description", expectedVault.Description),
+					resource.TestCheckResourceAttr("data.onepassword_vault.test", "name", expectedVault.Name),
 				),
 			},
 		},
 	})
 }
 
-const testAccExampleDataSourceConfig = `
-data "scaffolding_example" "test" {
-  configurable_attribute = "example"
+func testAccVaultDataSourceConfig(vault string) string {
+	return fmt.Sprintf(`
+data "onepassword_vault" "test" {
+  uuid = "%s"
+}`, vault)
 }
-`
