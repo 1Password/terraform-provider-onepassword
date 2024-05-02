6fbe23b21bfafceabfa766edcef69d0091d3f315
Eddy Filip <eddy.filip@agilebits.com>
2024-05-02 21:09:10 +0200

    Define OnePasswordItem resource schema and data model

--- internal/provider/onepassword_item_resource.go
+++ internal/provider/onepassword_item_resource.go
@@ -8,68 +8,274 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/hashicorp/terraform-plugin-framework-validators/int64validator"
+	"github.com/hashicorp/terraform-plugin-framework-validators/listvalidator"
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64default"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
+
+	"github.com/1Password/terraform-provider-onepassword/internal/onepassword"
 )
 
 // Ensure provider defined types fully satisfy framework interfaces.
-var _ resource.Resource = &ExampleResource{}
-var _ resource.ResourceWithImportState = &ExampleResource{}
+var _ resource.Resource = &OnePasswordItemResource{}
+var _ resource.ResourceWithImportState = &OnePasswordItemResource{}
 
-func NewExampleResource() resource.Resource {
-	return &ExampleResource{}
+func NewOnePasswordItemResource() resource.Resource {
+	return &OnePasswordItemResource{}
 }
 
-// ExampleResource defines the resource implementation.
-type ExampleResource struct {
-	client *http.Client
+// OnePasswordItemResource defines the resource implementation.
+type OnePasswordItemResource struct {
+	client onepassword.Client
 }
 
-// ExampleResourceModel describes the resource data model.
-type ExampleResourceModel struct {
-	ConfigurableAttribute types.String `tfsdk:"configurable_attribute"`
-	Defaulted             types.String `tfsdk:"defaulted"`
-	Id                    types.String `tfsdk:"id"`
+// OnePasswordItemResourceModel describes the resource data model.
+type OnePasswordItemResourceModel struct {
+	ID       types.String                          `tfsdk:"id"`
+	UUID     types.String                          `tfsdk:"uuid"`
+	Vault    types.String                          `tfsdk:"vault"`
+	Category types.String                          `tfsdk:"category"`
+	Title    types.String                          `tfsdk:"title"`
+	URL      types.String                          `tfsdk:"url"`
+	Hostname types.String                          `tfsdk:"hostname"`
+	Database types.String                          `tfsdk:"database"`
+	Port     types.String                          `tfsdk:"port"`
+	Type     types.String                          `tfsdk:"type"`
+	Tags     types.List                            `tfsdk:"tags"`
+	Username types.String                          `tfsdk:"username"`
+	Password types.String                          `tfsdk:"password"`
+	Section  []OnePasswordItemResourceSectionModel `tfsdk:"section"`
+	Recipe   []PasswordRecipeModel                 `tfsdk:"password_recipe"`
 }
 
-func (r *ExampleResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
-	resp.TypeName = req.ProviderTypeName + "_example"
+type PasswordRecipeModel struct {
+	Length  types.Int64 `tfsdk:"length"`
+	Letters types.Bool  `tfsdk:"letters"`
+	Digits  types.Bool  `tfsdk:"digits"`
+	Symbols types.Bool  `tfsdk:"symbols"`
 }
 
-func (r *ExampleResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
-	resp.Schema = schema.Schema{
-		// This description is used by the documentation generator and the language server.
-		MarkdownDescription: "Example resource",
+type OnePasswordItemResourceSectionModel struct {
+	ID    types.String                        `tfsdk:"id"`
+	Label types.String                        `tfsdk:"label"`
+	Field []OnePasswordItemResourceFieldModel `tfsdk:"field"`
+}
 
-		Attributes: map[string]schema.Attribute{
-			"configurable_attribute": schema.StringAttribute{
-				MarkdownDescription: "Example configurable attribute",
-				Optional:            true,
-			},
-			"defaulted": schema.StringAttribute{
-				MarkdownDescription: "Example configurable attribute with default value",
-				Optional:            true,
-				Computed:            true,
-				Default:             stringdefault.StaticString("example value when not configured"),
-			},
-			"id": schema.StringAttribute{
-				Computed:            true,
-				MarkdownDescription: "Example identifier",
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.UseStateForUnknown(),
+type OnePasswordItemResourceFieldModel struct {
+	ID      types.String          `tfsdk:"id"`
+	Label   types.String          `tfsdk:"label"`
+	Purpose types.String          `tfsdk:"purpose"`
+	Type    types.String          `tfsdk:"type"`
+	Value   types.String          `tfsdk:"value"`
+	Recipe  []PasswordRecipeModel `tfsdk:"password_recipe"`
+}
+
+func (r *OnePasswordItemResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_item"
+}
+
+func (r *OnePasswordItemResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+	// TODO: Consider using SingleNested
+	passwordRecipeBlockSchema := schema.ListNestedBlock{
+		MarkdownDescription: passwordRecipeDescription,
+		Validators: []validator.List{
+			listvalidator.SizeAtMost(1),
+		},
+		NestedObject: schema.NestedBlockObject{
+			Attributes: map[string]schema.Attribute{
+				"length": schema.Int64Attribute{
+					MarkdownDescription: passwordLengthDescription,
+					Optional:            true,
+					Computed:            true,
+					Default:             int64default.StaticInt64(32),
+					Validators: []validator.Int64{
+						int64validator.Between(1, 64),
+					},
+				},
+				"letters": schema.BoolAttribute{
+					MarkdownDescription: passwordLettersDescription,
+					Optional:            true,
+					Computed:            true,
+					Default:             booldefault.StaticBool(true),
+				},
+				"digits": schema.BoolAttribute{
+					MarkdownDescription: passwordDigitsDescription,
+					Optional:            true,
+					Computed:            true,
+					Default:             booldefault.StaticBool(true),
+				},
+				"symbols": schema.BoolAttribute{
+					MarkdownDescription: passwordSymbolsDescription,
+					Optional:            true,
+					Computed:            true,
+					Default:             booldefault.StaticBool(true),
 				},
 			},
 		},
 	}
+
+	resp.Schema = schema.Schema{
+		// This description is used by the documentation generator and the language server.
+		MarkdownDescription: "A 1Password Item.",
+
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				MarkdownDescription: terraformItemIDDescription,
+				Computed:            true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"uuid": schema.StringAttribute{
+				MarkdownDescription: itemUUIDDescription,
+				Computed:            true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"vault": schema.StringAttribute{
+				MarkdownDescription: vaultUUIDDescription,
+				Required:            true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"category": schema.StringAttribute{
+				MarkdownDescription: fmt.Sprintf(enumDescription, categoryDescription, categories),
+				Optional:            true,
+				Computed:            true,
+				Default:             stringdefault.StaticString("login"),
+				Validators: []validator.String{
+					stringvalidator.OneOfCaseInsensitive(categories...),
+				},
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"title": schema.StringAttribute{
+				MarkdownDescription: itemTitleDescription,
+				Optional:            true,
+			},
+			"url": schema.StringAttribute{
+				MarkdownDescription: urlDescription,
+				Optional:            true,
+			},
+			"hostname": schema.StringAttribute{
+				MarkdownDescription: dbHostnameDescription,
+				Optional:            true,
+			},
+			"database": schema.StringAttribute{
+				MarkdownDescription: dbDatabaseDescription,
+				Optional:            true,
+			},
+			"port": schema.StringAttribute{
+				MarkdownDescription: dbPortDescription,
+				Optional:            true,
+			},
+			"type": schema.StringAttribute{
+				MarkdownDescription: fmt.Sprintf(enumDescription, dbTypeDescription, dbTypes),
+				Optional:            true,
+				Validators: []validator.String{
+					stringvalidator.OneOfCaseInsensitive(dbTypes...),
+				},
+			},
+			"tags": schema.ListAttribute{
+				MarkdownDescription: tagsDescription,
+				ElementType:         types.StringType,
+				Optional:            true,
+			},
+			"username": schema.StringAttribute{
+				MarkdownDescription: usernameDescription,
+				Optional:            true,
+			},
+			"password": schema.StringAttribute{
+				MarkdownDescription: passwordDescription,
+				Optional:            true,
+				Computed:            true,
+				Sensitive:           true,
+			},
+		},
+		Blocks: map[string]schema.Block{
+			"section": schema.ListNestedBlock{
+				MarkdownDescription: sectionsDescription,
+				NestedObject: schema.NestedBlockObject{
+					Attributes: map[string]schema.Attribute{
+						"id": schema.StringAttribute{
+							MarkdownDescription: sectionIDDescription,
+							Computed:            true,
+							PlanModifiers: []planmodifier.String{
+								stringplanmodifier.UseStateForUnknown(),
+							},
+						},
+						"label": schema.StringAttribute{
+							MarkdownDescription: sectionLabelDescription,
+							Required:            true,
+						},
+					},
+					Blocks: map[string]schema.Block{
+						"field": schema.ListNestedBlock{
+							MarkdownDescription: sectionFieldsDescription,
+							NestedObject: schema.NestedBlockObject{
+								Attributes: map[string]schema.Attribute{
+									"id": schema.StringAttribute{
+										MarkdownDescription: fieldIDDescription,
+										Optional:            true,
+										Computed:            true,
+										PlanModifiers: []planmodifier.String{
+											stringplanmodifier.UseStateForUnknown(),
+										},
+									},
+									"label": schema.StringAttribute{
+										MarkdownDescription: fieldLabelDescription,
+										Required:            true,
+									},
+									"purpose": schema.StringAttribute{
+										MarkdownDescription: fmt.Sprintf(enumDescription, fieldPurposeDescription, fieldPurposes),
+										Optional:            true,
+										Validators: []validator.String{
+											stringvalidator.OneOfCaseInsensitive(fieldPurposes...),
+										},
+									},
+									"type": schema.StringAttribute{
+										MarkdownDescription: fmt.Sprintf(enumDescription, fieldTypeDescription, fieldTypes),
+										Optional:            true,
+										Computed:            true,
+										Default:             stringdefault.StaticString("STRING"),
+										Validators: []validator.String{
+											stringvalidator.OneOfCaseInsensitive(fieldTypes...),
+										},
+									},
+									"value": schema.StringAttribute{
+										MarkdownDescription: fieldValueDescription,
+										Optional:            true,
+										Computed:            true,
+										Sensitive:           true,
+									},
+								},
+								Blocks: map[string]schema.Block{
+									"password_recipe": passwordRecipeBlockSchema,
+								},
+							},
+						},
+					},
+				},
+			},
+			"password_recipe": passwordRecipeBlockSchema,
+		},
+	}
 }
 
-func (r *ExampleResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+func (r *OnePasswordItemResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
 	// Prevent panic if the provider has not been configured.
 	if req.ProviderData == nil {
 		return
@@ -89,8 +295,8 @@ func (r *ExampleResource) Configure(ctx context.Context, req resource.ConfigureR
 	r.client = client
 }
 
-func (r *ExampleResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
-	var data ExampleResourceModel
+func (r *OnePasswordItemResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var data OnePasswordItemResourceModel
 
 	// Read Terraform plan data into the model
 	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
@@ -109,7 +315,7 @@ func (r *ExampleResource) Create(ctx context.Context, req resource.CreateRequest
 
 	// For the purposes of this example code, hardcoding a response value to
 	// save into the Terraform state.
-	data.Id = types.StringValue("example-id")
+	data.ID = types.StringValue("example-id")
 
 	// Write logs using the tflog package
 	// Documentation: https://terraform.io/plugin/log
@@ -119,8 +325,8 @@ func (r *ExampleResource) Create(ctx context.Context, req resource.CreateRequest
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
 
-func (r *ExampleResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
-	var data ExampleResourceModel
+func (r *OnePasswordItemResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	var data OnePasswordItemResourceModel
 
 	// Read Terraform prior state data into the model
 	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
@@ -141,8 +347,8 @@ func (r *ExampleResource) Read(ctx context.Context, req resource.ReadRequest, re
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
 
-func (r *ExampleResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
-	var data ExampleResourceModel
+func (r *OnePasswordItemResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var data OnePasswordItemResourceModel
 
 	// Read Terraform plan data into the model
 	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
@@ -163,8 +369,8 @@ func (r *ExampleResource) Update(ctx context.Context, req resource.UpdateRequest
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
 
-func (r *ExampleResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
-	var data ExampleResourceModel
+func (r *OnePasswordItemResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var data OnePasswordItemResourceModel
 
 	// Read Terraform prior state data into the model
 	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
@@ -182,6 +388,6 @@ func (r *ExampleResource) Delete(ctx context.Context, req resource.DeleteRequest
 	// }
 }
 
-func (r *ExampleResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+func (r *OnePasswordItemResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
 	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
 }
--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -153,7 +153,7 @@ func (p *OnePasswordProvider) Configure(ctx context.Context, req provider.Config
 
 func (p *OnePasswordProvider) Resources(ctx context.Context) []func() resource.Resource {
 	return []func() resource.Resource{
-		NewExampleResource,
+		NewOnePasswordItemResource,
 	}
 }
 
