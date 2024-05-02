ef19767ffc76ff93f51bff94c1ef97968674ac41
Eddy Filip <eddy.filip@agilebits.com>
2024-05-02 23:51:53 +0200

    Configure the testing provider
    Co-authored-by: jillianwilson <jillian.morgan@agilebits.com>

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -4,7 +4,7 @@
 package provider
 
 import (
-	"testing"
+	"fmt"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
@@ -15,11 +15,13 @@ import (
 // CLI command executed to create a provider server to which the CLI can
 // reattach.
 var testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
-	"scaffolding": providerserver.NewProtocol6WithError(New("test")()),
+	"onepassword": providerserver.NewProtocol6WithError(New("test")()),
 }
 
-func testAccPreCheck(t *testing.T) {
-	// You can add code here to run prior to any test case execution, for example assertions
-	// about the appropriate environment variables being set are common to see in a pre-check
-	// function.
+func testAccProviderConfig(url string) string {
+	return fmt.Sprintf(`
+	  provider "onepassword" {
+		url = "%s"
+		token = "<PASSWORD>"
+	  }`, url)
 }
