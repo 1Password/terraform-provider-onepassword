beedbb2d278c659116fba64907d62b2ebd15b03f
Eddy Filip <eddy.filip@agilebits.com>
2024-05-02 19:14:50 +0200

    Remove provider function scaffolding
    Currently, the provider doesn't implement any functions. Therefore, these files are removed.

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -79,9 +79,7 @@ func (p *ScaffoldingProvider) DataSources(ctx context.Context) []func() datasour
 }
 
 func (p *ScaffoldingProvider) Functions(ctx context.Context) []func() function.Function {
-	return []func() function.Function{
-		NewExampleFunction,
-	}
+	return []func() function.Function{}
 }
 
 func New(version string) func() provider.Provider {
