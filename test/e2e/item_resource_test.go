2bbc330333d0cccc62d829d06b8e788c856253b6
Jill Regan <jillian.jnr@hotmail.com>
2025-10-28 09:41:22 -0400

    Add CRUD tests for resource items

--- test/e2e/item_resource_test.go
+++ test/e2e/item_resource_test.go
@@ -1,79 +1,305 @@
 package integration
 
 import (
+	"fmt"
+	"regexp"
 	"testing"
 
-	"github.com/1Password/terraform-provider-onepassword/v2/test/e2e/config"
+	op "github.com/1Password/connect-sdk-go/onepassword"
 	tfconfig "github.com/1Password/terraform-provider-onepassword/v2/test/e2e/terraform/config"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
-	"github.com/hashicorp/terraform-plugin-testing/terraform"
 )
 
-func TestAccItemResource(t *testing.T) {
-	serviceAccountToken, err := config.GetServiceAccountToken()
-	if err != nil {
-		t.Fatalf("Failed to get test config: %v", err)
-	}
+type testItemResource struct {
+	Title string
+	Attrs map[string]string
+}
 
-	testCases := []struct {
-		name               string
-		itemResourceConfig tfconfig.ItemResource
-		expectedAttributes map[string]string
-		validateFunc       func(*terraform.State) error
-	}{
-		{
-			name: "CreateLogin",
-			itemResourceConfig: tfconfig.ItemResource{
-				Auth: tfconfig.AuthConfig{
-					ServiceAccountToken: serviceAccountToken,
-				},
-				Params: map[string]string{
-					"vault":      "t7dnwbjh6nlyw475wl3m442sdi",
-					"title":      "Test Login Item 2",
-					"category":   "login",
-					"username":   "testuser@example.com",
-					"url":        "https://example.com",
-					"password":   "testpassword",
-					"note_value": "Test note",
-				},
-				Tags: []string{"tag1", "tag2"},
-			},
-			expectedAttributes: map[string]string{
-				"title":      "Test Login Item 2",
-				"category":   "login",
-				"username":   "testuser@example.com",
-				"url":        "https://example.com",
-				"password":   "testpassword",
-				"note_value": "Test note",
-				"tags.0":     "tag1",
-				"tags.1":     "tag2",
-			},
+var testItemsToCreate = map[op.ItemCategory]testItemResource{
+	op.Login: {
+		Title: "Test Login Create",
+		Attrs: map[string]string{
+			"vault":      "bbucuyq2nn4fozygwttxwizpcy",
+			"category":   "login",
+			"username":   "testuser@example.com",
+			"password":   "testPassword",
+			"url":        "https://example.com",
+			"note_value": "Test login note",
 		},
+	},
+	op.Password: {
+		Title: "Test Password Create",
+		Attrs: map[string]string{
+			"vault":    "bbucuyq2nn4fozygwttxwizpcy",
+			"category": "password",
+			"password": "testPassword",
+		},
+	},
+	op.Database: {
+		Title: "Test Database Create",
+		Attrs: map[string]string{
+			"vault":    "bbucuyq2nn4fozygwttxwizpcy",
+			"category": "database",
+			"username": "testUsername",
+			"password": "testPassword",
+			"database": "testDatabase",
+			"port":     "3306",
+			"type":     "mysql",
+		},
+	},
+	op.SecureNote: {
+		Title: "Test Secure Note Create",
+		Attrs: map[string]string{
+			"vault":      "bbucuyq2nn4fozygwttxwizpcy",
+			"category":   "secure_note",
+			"note_value": "This is a test secure note",
+		},
+	},
+}
+
+func TestAccItemResourceCreate(t *testing.T) {
+	testCases := []struct {
+		category op.ItemCategory
+		name     string
+	}{
+		{op.Login, "Login"},
+		{op.Password, "Password"},
+		{op.Database, "Database"},
+		{op.SecureNote, "SecureNote"},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
+			item := testItemsToCreate[tc.category]
 			resourceBuilder := tfconfig.CreateItemResourceConfigBuilder()
 
-			checks := []resource.TestCheckFunc{
-				resource.TestCheckResourceAttr("onepassword_item.test_item", "vault", "t7dnwbjh6nlyw475wl3m442sdi"),
+			config := make(map[string]string)
+			config["title"] = item.Title
+			for k, v := range item.Attrs {
+				config[k] = v
 			}
 
-			for attr, expectedValue := range tc.expectedAttributes {
+			checks := []resource.TestCheckFunc{
+				resource.TestCheckResourceAttr("onepassword_item.test_item", "title", item.Title),
+				resource.TestCheckResourceAttrSet("onepassword_item.test_item", "id"),
+				resource.TestCheckResourceAttrSet("onepassword_item.test_item", "uuid"),
+			}
+
+			for attr, expectedValue := range item.Attrs {
 				checks = append(checks, resource.TestCheckResourceAttr("onepassword_item.test_item", attr, expectedValue))
 			}
-
-			//	checks = append(checks, resource.TestCheckResourceAttrSet("onepassword_item.test_item", "uuid"))
 
 			resource.Test(t, resource.TestCase{
 				ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 				Steps: []resource.TestStep{
 					{
 						Config: resourceBuilder(
-							tfconfig.ProviderAuthWithServiceAccount(tc.itemResourceConfig.Auth),
-							tfconfig.ItemResourceConfig(tc.itemResourceConfig.Params, tc.itemResourceConfig.Tags),
+							tfconfig.ProviderConfig(),
+							tfconfig.ItemResourceConfig(config),
 						),
 						Check: resource.ComposeAggregateTestCheckFunc(checks...),
+					},
+				},
+			})
+		})
+	}
+}
+
+var testItemsUpdatedAttrs = map[op.ItemCategory]map[string]string{
+	op.Login: {
+		"username":   "updateduser@example.com",
+		"password":   "updatedPassword",
+		"url":        "https://updated-example.com",
+		"note_value": "Updated login note",
+	},
+	op.Password: {
+		"password": "updatedPassword",
+	},
+	op.Database: {
+		"username": "updatedUsername",
+		"password": "updatedPassword",
+		"database": "updatedDatabase",
+		"port":     "5432",
+		"type":     "postgresql",
+	},
+	op.SecureNote: {
+		"note_value": "This is an updated secure note",
+	},
+}
+
+func TestAccItemResourceUpdate(t *testing.T) {
+	testCases := []struct {
+		category op.ItemCategory
+		name     string
+	}{
+		{op.Login, "Login"},
+		{op.Password, "Password"},
+		{op.Database, "Database"},
+		{op.SecureNote, "SecureNote"},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			item := testItemsToCreate[tc.category]
+
+			initialConfig := make(map[string]string)
+			initialConfig["title"] = item.Title
+			for k, v := range item.Attrs {
+				initialConfig[k] = v
+			}
+
+			updatedConfig := make(map[string]string)
+			updatedConfig["title"] = item.Title
+			for k, v := range item.Attrs {
+				updatedConfig[k] = v
+			}
+
+			for k, v := range testItemsUpdatedAttrs[tc.category] {
+				updatedConfig[k] = v
+			}
+
+			initialChecks := []resource.TestCheckFunc{
+				resource.TestCheckResourceAttr("onepassword_item.test_item", "title", item.Title),
+				resource.TestCheckResourceAttrSet("onepassword_item.test_item", "id"),
+			}
+			for attr, expectedValue := range item.Attrs {
+				initialChecks = append(initialChecks, resource.TestCheckResourceAttr("onepassword_item.test_item", attr, expectedValue))
+			}
+
+			updatedChecks := []resource.TestCheckFunc{
+				resource.TestCheckResourceAttr("onepassword_item.test_item", "title", item.Title),
+			}
+			for attr, expectedValue := range testItemsUpdatedAttrs[tc.category] {
+				updatedChecks = append(updatedChecks, resource.TestCheckResourceAttr("onepassword_item.test_item", attr, expectedValue))
+			}
+
+			resource.Test(t, resource.TestCase{
+				ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+				Steps: []resource.TestStep{
+					{
+						Config: tfconfig.CreateItemResourceConfigBuilder()(
+							tfconfig.ProviderConfig(),
+							tfconfig.ItemResourceConfig(initialConfig),
+						),
+						Check: resource.ComposeAggregateTestCheckFunc(initialChecks...),
+					},
+					{
+						Config: tfconfig.CreateItemResourceConfigBuilder()(
+							tfconfig.ProviderConfig(),
+							tfconfig.ItemResourceConfig(updatedConfig),
+						),
+						Check: resource.ComposeAggregateTestCheckFunc(updatedChecks...),
+					},
+				},
+			})
+		})
+	}
+}
+
+func TestAccItemResourceRead(t *testing.T) {
+	testCases := []struct {
+		category op.ItemCategory
+		name     string
+	}{
+		{op.Login, "Login"},
+		{op.Password, "Password"},
+		{op.Database, "Database"},
+		{op.SecureNote, "SecureNote"},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			item := testItems[tc.category]
+			resourceBuilder := tfconfig.CreateItemResourceConfigBuilder()
+
+			config := make(map[string]string)
+			config["vault"] = testVaultID
+			config["title"] = item.Title
+			for k, v := range item.Attrs {
+				config[k] = v
+			}
+
+			checks := []resource.TestCheckFunc{
+				resource.TestCheckResourceAttr("onepassword_item.test_item", "title", item.Title),
+				resource.TestCheckResourceAttr("onepassword_item.test_item", "uuid", item.UUID),
+				resource.TestCheckResourceAttrSet("onepassword_item.test_item", "id"),
+			}
+			for attr, expectedValue := range item.Attrs {
+				checks = append(checks, resource.TestCheckResourceAttr("onepassword_item.test_item", attr, expectedValue))
+			}
+
+			resource.Test(t, resource.TestCase{
+				ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+				Steps: []resource.TestStep{
+					{
+						Config: resourceBuilder(
+							tfconfig.ProviderConfig(),
+							tfconfig.ItemResourceConfig(config),
+						),
+						ResourceName:      "onepassword_item.test_item",
+						ImportState:       true,
+						ImportStateId:     fmt.Sprintf("vaults/%s/items/%s", testVaultID, item.UUID),
+						ImportStateVerify: false,
+						Check:             resource.ComposeAggregateTestCheckFunc(checks...),
+					},
+				},
+			})
+		})
+	}
+}
+
+func TestAccItemResourceDelete(t *testing.T) {
+	testCases := []struct {
+		category op.ItemCategory
+		name     string
+	}{
+		{op.Login, "Login"},
+		{op.Password, "Password"},
+		{op.Database, "Database"},
+		{op.SecureNote, "SecureNote"},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			item := testItemsToCreate[tc.category]
+
+			config := make(map[string]string)
+			config["title"] = item.Title
+			for k, v := range item.Attrs {
+				config[k] = v
+			}
+
+			checks := []resource.TestCheckFunc{
+				resource.TestCheckResourceAttr("onepassword_item.test_item", "title", item.Title),
+				resource.TestCheckResourceAttrSet("onepassword_item.test_item", "id"),
+			}
+
+			resource.Test(t, resource.TestCase{
+				ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+				Steps: []resource.TestStep{
+					{
+						Config: tfconfig.CreateItemResourceConfigBuilder()(
+							tfconfig.ProviderConfig(),
+							tfconfig.ItemResourceConfig(config),
+						),
+						Check: resource.ComposeAggregateTestCheckFunc(checks...),
+					},
+					{
+						Config: tfconfig.CreateItemResourceConfigBuilder()(
+							tfconfig.ProviderConfig(),
+						),
+					},
+					{
+						Config: tfconfig.CreateItemResourceConfigBuilder()(
+							tfconfig.ProviderConfig(),
+							tfconfig.ItemDataSourceConfig(
+								map[string]string{
+									"vault": testVaultID,
+									"title": item.Title,
+								},
+							),
+						),
+						ExpectError: regexp.MustCompile("Unable to read item"),
 					},
 				},
 			})
--- test/e2e/terraform/config/item_resource_config.go
+++ test/e2e/terraform/config/item_resource_config.go
@@ -3,29 +3,16 @@ package terraform
 import "fmt"
 
 type ItemResource struct {
-	Auth   AuthConfig
 	Params map[string]string
 	Tags   []string
 }
 
-func ItemResourceConfig(params map[string]string, tags []string) func() string {
+func ItemResourceConfig(params map[string]string) func() string {
 	return func() string {
 		dataSourceStr := `resource "onepassword_item" "test_item" {`
 
 		for key, value := range params {
 			dataSourceStr += fmt.Sprintf("\n  %s = %q", key, value)
-		}
-
-		// Add tags if they exist
-		if len(tags) > 0 {
-			dataSourceStr += "\n  tags = ["
-			for i, tag := range tags {
-				if i > 0 {
-					dataSourceStr += ", "
-				}
-				dataSourceStr += fmt.Sprintf("%q", tag)
-			}
-			dataSourceStr += "]"
 		}
 
 		dataSourceStr += "\n}"
