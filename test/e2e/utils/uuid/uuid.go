89da259dca2e476be90da4b7bfbbc5e394f1d3b0
Jill Regan <jillian.jnr@hotmail.com>
2025-10-31 11:39:24 -0400

    Add error handeling for uuid checks

--- test/e2e/utils/uuid/uuid.go
+++ test/e2e/utils/uuid/uuid.go
@@ -11,8 +11,17 @@ import (
 // CaptureItemUUID captures the UUID of a resource item
 func CaptureItemUUID(t *testing.T, resourceName string, uuidPtr *string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
-		rs := s.RootModule().Resources[resourceName]
+		rs, ok := s.RootModule().Resources[resourceName]
+
+		if !ok {
+			return fmt.Errorf("resource not found: %s", resourceName)
+		}
+
 		*uuidPtr = rs.Primary.Attributes["uuid"]
+
+		if *uuidPtr == "" {
+			return fmt.Errorf("UUID is empty")
+		}
 
 		return nil
 	}
@@ -21,8 +30,17 @@ func CaptureItemUUID(t *testing.T, resourceName string, uuidPtr *string) resourc
 // VerifyItemUUIDUnchanged verifies that the resource UUID matches the expected UUID
 func VerifyItemUUIDUnchanged(t *testing.T, resourceName string, expectedUUID *string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
-		rs := s.RootModule().Resources[resourceName]
+		rs, ok := s.RootModule().Resources[resourceName]
+
+		if !ok {
+			return fmt.Errorf("resource not found: %s", resourceName)
+		}
+
 		currentUUID := rs.Primary.Attributes["uuid"]
+
+		if currentUUID == "" {
+			return fmt.Errorf("UUID is empty")
+		}
 
 		if currentUUID != *expectedUUID {
 			return fmt.Errorf("UUID changed from %s to %s - resource was replaced instead of updated", *expectedUUID, currentUUID)
