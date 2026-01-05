207c889f9fd4ab2e59c703b91772f1376b4cc550
Jill Regan <jillian.jnr@hotmail.com>
2026-01-05 17:03:39 -0500

    Add back purpose

--- internal/provider/onepassword_item_data_source.go
+++ internal/provider/onepassword_item_data_source.go
@@ -72,11 +72,11 @@ type OnePasswordItemSectionModel struct {
 }
 
 type OnePasswordItemFieldModel struct {
-	ID    types.String `tfsdk:"id"`
-	Label types.String `tfsdk:"label"`
-
-	Type  types.String `tfsdk:"type"`
-	Value types.String `tfsdk:"value"`
+	ID      types.String `tfsdk:"id"`
+	Label   types.String `tfsdk:"label"`
+	Purpose types.String `tfsdk:"purpose"`
+	Type    types.String `tfsdk:"type"`
+	Value   types.String `tfsdk:"value"`
 }
 
 func (d *OnePasswordItemDataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
@@ -226,6 +226,10 @@ func (d *OnePasswordItemDataSource) Schema(ctx context.Context, req datasource.S
 										MarkdownDescription: fieldLabelDescription,
 										Computed:            true,
 									},
+									"purpose": schema.StringAttribute{
+										MarkdownDescription: fmt.Sprintf(enumDescription, fieldPurposeDescription, fieldPurposes),
+										Computed:            true,
+									},
 									"type": schema.StringAttribute{
 										MarkdownDescription: fmt.Sprintf(enumDescription, fieldTypeDescription, fieldTypes),
 										Computed:            true,
@@ -320,10 +324,11 @@ func (d *OnePasswordItemDataSource) Read(ctx context.Context, req datasource.Rea
 		for _, f := range item.Fields {
 			if f.SectionID != "" && f.SectionID == s.ID {
 				section.Field = append(section.Field, OnePasswordItemFieldModel{
-					ID:    types.StringValue(f.ID),
-					Label: types.StringValue(f.Label),
-					Type:  types.StringValue(string(f.Type)),
-					Value: types.StringValue(f.Value),
+					ID:      types.StringValue(f.ID),
+					Label:   types.StringValue(f.Label),
+					Purpose: types.StringValue(string(f.Purpose)),
+					Type:    types.StringValue(string(f.Type)),
+					Value:   types.StringValue(f.Value),
 				})
 			}
 		}
