f8db2c882631b8dc3a9a5946d603fba6e727a933
Volodymyr Zotov <volodymyr.zotov@agilebits.com>
2026-01-09 16:13:51 -0600

    Use vault ID instead of vault title

--- test/e2e/utils/vault/vault.go
+++ test/e2e/utils/vault/vault.go
@@ -1,16 +1,10 @@
 package vault
 
 import (
-	"sync"
 	"testing"
-)
-
-var (
-	testVaultIDOnce sync.Once
-	testVaultID     string
 )
 
 // GetTestVaultID returns the vault ID by querying by name once and caching the result.
 func GetTestVaultID(t *testing.T) string {
-	return "terraform-provider-acceptance-tests"
+	return "bbucuyq2nn4fozygwttxwizpcy" // "terraform-provider-acceptance-tests" vault ID
 }
