8cf833ac06d205c24dd7f9d354b1ac8d47ae1610
Volodymyr Zotov <volodymyr.zotov@agilebits.com>
2026-01-09 12:53:30 -0600

    Return hardcoded vaule name so it doesn't make additional reqiest

--- test/e2e/utils/vault/vault.go
+++ test/e2e/utils/vault/vault.go
@@ -1,12 +1,8 @@
 package vault
 
 import (
-	"context"
-	"os"
 	"sync"
 	"testing"
-
-	"github.com/1Password/terraform-provider-onepassword/v2/test/e2e/utils/client"
 )
 
 var (
@@ -16,31 +12,5 @@ var (
 
 // GetTestVaultID returns the vault ID by querying by name once and caching the result.
 func GetTestVaultID(t *testing.T) string {
-	testVaultIDOnce.Do(func() {
-		vaultName := os.Getenv("OP_TEST_VAULT_NAME")
-		if vaultName == "" {
-			vaultName = "terraform-provider-acceptance-tests"
-		}
-
-		ctx := context.Background()
-		client, err := client.CreateTestClient(ctx)
-		if err != nil {
-			t.Fatalf("failed to create test client: %v", err)
-		}
-
-		vaults, err := client.GetVaultsByTitle(ctx, vaultName)
-		if err != nil {
-			t.Fatalf("failed to get vault by name %q: %v", vaultName, err)
-		}
-
-		if len(vaults) == 0 {
-			t.Fatalf("no vault found with name %q", vaultName)
-		}
-		if len(vaults) > 1 {
-			t.Fatalf("multiple vaults found with name %q", vaultName)
-		}
-
-		testVaultID = vaults[0].ID
-	})
-	return testVaultID
+	return "terraform-provider-acceptance-tests"
 }
