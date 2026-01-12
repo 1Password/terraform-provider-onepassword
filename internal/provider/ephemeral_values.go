0ad1ef523dea24de15242d3446b72cb7554c8475
Jill Regan <jillian.jnr@hotmail.com>
2026-01-12 13:38:30 -0500

    Remove commented uout method

--- internal/provider/ephemeral_values.go
+++ internal/provider/ephemeral_values.go
@@ -21,53 +21,6 @@ func clearWriteOnlyFieldFromState(version types.Int64, stateValue *types.String)
 	}
 }
 
-// func (r *OnePasswordItemResource) handleWriteOnlyFieldUpdate(
-// 	ctx context.Context,
-// 	configVersion types.Int64,
-// 	stateVersion types.Int64,
-// 	woValue types.String,
-// 	planValue *types.String,
-// 	planID types.String,
-// 	fieldPurpose model.ItemFieldPurpose,
-// 	fieldName string,
-// ) error {
-// 	if configVersion.IsNull() {
-// 		return nil
-// 	}
-
-// 	configVer := configVersion.ValueInt64()
-// 	stateVer := int64(0)
-// 	if !stateVersion.IsNull() {
-// 		stateVer = stateVersion.ValueInt64()
-// 	}
-
-// 	if configVer > stateVer {
-// 		// Version increased - use new write-only value
-// 		*planValue = woValue
-// 	} else {
-// 		// Version unchanged or decreased - preserve existing value by reading current item
-// 		vaultUUID, itemUUID := vaultAndItemUUID(planID.ValueString())
-// 		currentItem, err := r.client.GetItem(ctx, itemUUID, vaultUUID)
-// 		if err != nil {
-// 			return fmt.Errorf("Could not read item '%s' from vault '%s' to preserve %s, got error: %s", itemUUID, vaultUUID, fieldName, err)
-// 		}
-// 		// Extract field from current item
-// 		fieldFound := false
-// 		for _, f := range currentItem.Fields {
-// 			if f.Purpose == fieldPurpose {
-// 				*planValue = types.StringValue(f.Value)
-// 				fieldFound = true
-// 				break
-// 			}
-// 		}
-// 		// Field not found (user removed it in 1Password), sync to that state
-// 		if !fieldFound {
-// 			*planValue = types.StringNull()
-// 		}
-// 	}
-// 	return nil
-// }
-
 // handleWriteOnlyFieldUpdates processes all write-only field updates,
 func handleWriteOnlyFieldUpdates(
 	config *OnePasswordItemResourceModel,
